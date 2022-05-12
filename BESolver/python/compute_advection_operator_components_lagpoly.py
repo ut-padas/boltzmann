Investigating Eulerian approach with orthogonal polynomials. Fixed derivatives for maxwell polynomials, now advection error does not grow with number of radial basis functions.

BESolver/python/compute_advection_operator_components_lagpoly.py
@@ -3,8 +3,8 @@ import numpy as np
 import scipy as scipy
 from maxpoly import *
 
-lmax = 33
-Nr_max = 33
+lmax = 31
+Nr_max = 150
 CpsiDphiP = np.zeros([lmax,Nr_max+1,Nr_max+1])
 CphiDpsiP = np.zeros([lmax,Nr_max+1,Nr_max+1])
 CpsiDphiM = np.zeros([lmax,Nr_max+1,Nr_max+1])
@@ -14,7 +14,7 @@ norm = lambda k,alpha: scipy.special.gamma(k+alpha+1)/factorial(max(k,0))
 lagp = lambda k,alpha,x2: scipy.special.eval_genlaguerre(k, alpha, x2)*np.sqrt(2./norm(k,alpha))
 xlagp_der = lambda k,alpha,x2: -2.*x2*scipy.special.eval_genlaguerre(k-1, alpha+1, x2)*np.sqrt(2./norm(k,alpha))
 
-gauss_q_order_proj = 256
+gauss_q_order_proj = 200
 [xg, wg] = maxpolygauss(gauss_q_order_proj)
 
 xg2 = xg**2
@@ -23,25 +23,26 @@ for q in range(lmax):
     
     for p in range(Nr_max+1):
         subintegrand = q*lagp(p, q+.5, xg2) + xlagp_der(p, q+.5, xg2)
-        for k in range(Nr_max+1):
+        for k in range(max(0,p-1),Nr_max+1):
             integrand = xg**(2*q)*lagp(k, q+1.5, xg2)*subintegrand
-            CphiDpsiP[q,p,k] = sum(integrand*wg)
+            CphiDpsiP[q,p,k] = np.dot(integrand,wg)
 
+        for k in range(0,p+1):
             if q > 0:
                 integrand = xg**(2*q-2)*lagp(k, q-0.5, xg2)*subintegrand
-                CphiDpsiM[q,p,k] = sum(integrand*wg)
+                CphiDpsiM[q,p,k] = np.dot(integrand,wg)
 
     for k in range(Nr_max+1):
         subintegrand = ((q+1)-2*xg2)*lagp(k,q+1.5,xg2) + xlagp_der(k,q+1.5,xg2)
-        for p in range(Nr_max+1):
+        for p in range(min(Nr_max,k+1)+1):
             integrand = xg**(2*q)*lagp(p,q+.5,xg2)*subintegrand
-            CpsiDphiP[q,p,k] = sum(integrand*wg)
+            CpsiDphiP[q,p,k] = np.dot(integrand,wg)
 
         if q > 0:
             subintegrand = ((q-1)-2*xg2)*lagp(k,q-.5,xg2) + xlagp_der(k,q-.5,xg2)
-            for p in range(Nr_max+1):
+            for p in range(k, Nr_max+1):
                 integrand = xg**(2*q-2)*lagp(p,q+.5,xg2)*subintegrand
-                CpsiDphiM[q,p,k] = sum(integrand*wg)
+                CpsiDphiM[q,p,k] = np.dot(integrand,wg)
 
     print(q)
 

BESolver/python/maxpoly.py
@@ -5,19 +5,25 @@ from scipy.special import gamma
 # organized as [a00, a10, a11, a20, a21, a22, ... ], that is 
 # coefficients of n-degree polynomial are given by max_poly_coeffs[n*(n+1)/2]
 
-maxpoly_nmax    = 554 # max degree allowed
-maxpoly_data    = np.genfromtxt('polynomials/maxpoly_upto555.dat',delimiter=',')
+maxpoly_nmax    = 299 # max degree allowed
+# maxpoly_data    = np.genfromtxt('polynomials/maxpoly_upto555.dat',delimiter=',')
 # maxpoly_coeffs  = maxpoly_data[:,0]
-maxpoly_nodes   = maxpoly_data[:,0]
-maxpoly_weights = maxpoly_data[:,1]
+# maxpoly_nodes   = maxpoly_data[:,0]
+# maxpoly_weights = maxpoly_data[:,1]
 
-maxpoly_rec_data = np.genfromtxt('polynomials/maxpoly_upto555_recursive.dat',delimiter=',')
-maxpoly_rec_a    = maxpoly_rec_data[:,0]
-maxpoly_rec_b    = maxpoly_rec_data[:,1]
+# maxpoly_rec_data = np.genfromtxt('polynomials/maxpoly_upto555_recursive.dat',delimiter=',')
+# maxpoly_rec_a    = maxpoly_rec_data[:,0]
+# maxpoly_rec_b    = maxpoly_rec_data[:,1]
 # maxpoly_rec_n    = maxpoly_rec_data[:,2]
 
-maxpoly_alpha = np.genfromtxt('polynomials/maxpoly_alpha_upto300_new.dat',delimiter=',')
-maxpoly_beta = np.genfromtxt('polynomials/maxpoly_beta_upto300_new.dat',delimiter=',')
+# maxpoly_alpha = np.genfromtxt('polynomials/maxpoly_alpha_upto300_new.dat',delimiter=',')
+# maxpoly_beta = np.genfromtxt('polynomials/maxpoly_beta_upto300_new.dat',delimiter=',')
+
+maxpoly_alpha = np.genfromtxt('polynomials/maxpoly_alpha_300_65.dat',delimiter=',')
+maxpoly_beta = np.genfromtxt('polynomials/maxpoly_beta_300_65.dat',delimiter=',')
+maxpoly_nodes   = np.genfromtxt('polynomials/maxpoly_nodes_300_65.dat',delimiter=',')
+maxpoly_weights = np.genfromtxt('polynomials/maxpoly_weights_300_65.dat',delimiter=',')
+maxpoly_deriv = np.genfromtxt('polynomials/maxpoly_deriv_300_65.dat',delimiter=',')
 
 def idx_s(p):
     return int(p*(p+1)/2)
@@ -25,22 +31,22 @@ def idx_s(p):
 def idx_e(p):
     return int(p*(p+1)/2+p)
 
-def maxpolygauss(p):
-    return [maxpoly_nodes[idx_s(p):idx_e(p)+1], maxpoly_weights[idx_s(p):idx_e(p)+1]]
+def maxpolygauss(p, G=2):
+    return [maxpoly_nodes[G, idx_s(p):idx_e(p)+1], maxpoly_weights[G, idx_s(p):idx_e(p)+1]]
 
-def maxpolyeval_naive(x, p):
+# def maxpolyeval_naive(x, p):
 
-    if p == 0:
-        return np.ones(np.shape(x))
+#     if p == 0:
+#         return np.ones(np.shape(x))
 
-    result = maxpoly_coeffs[idx_e(p)]
-    for i in range(p):
-        result = result*x + maxpoly_coeffs[idx_e(p)-i-1]
-    return result
+#     result = maxpoly_coeffs[idx_e(p)]
+#     for i in range(p):
+#         result = result*x + maxpoly_coeffs[idx_e(p)-i-1]
+#     return result
 
 def maxpolyeval(G, x, p):
 
-    idx = int(G/2)
+    idx = G
 
     g1 = gamma((1.+G)/2.)
     g2 = gamma((2.+G)/2.)
@@ -64,7 +70,7 @@ def maxpolyeval(G, x, p):
 
 def maxpolyserieseval(G, x, coeffs):
 
-    idx = int(G/2)
+    idx = G
 
     g1 = gamma((1.+G)/2.)
     g2 = gamma((2.+G)/2.)
@@ -88,47 +94,60 @@ def maxpolyserieseval(G, x, coeffs):
 
     return coeffs[0]*np.sqrt(2./g1) + Bn*(x-g2/g1)*np.sqrt(2)/np.sqrt(g3-g2**2/g1) - np.sqrt(maxpoly_beta[idx,1]/maxpoly_beta[idx,2])*Bnp1*np.sqrt(2./g1)
 
-def maxpolyeval_non_norm(x, p):
+# def maxpolyeval_non_norm(x, p):
 
-    if p == 0:
-        return np.ones(np.shape(x))
+#     if p == 0:
+#         return np.ones(np.shape(x))
 
-    Bn   = 0
-    Bnp1 = 0
-    Bnp2 = 0
-    for i in range(p,0,-1):
-        Bnp2 = Bnp1
-        Bnp1 = Bn
-        if i == p:
-            Bn = np.ones(np.shape(x)) + (x-maxpoly_rec_a[i])*Bnp1 - maxpoly_rec_b[i+1]*Bnp2
-        else:
-            Bn = (x-maxpoly_rec_a[i])*Bnp1 - maxpoly_rec_b[i+1]*Bnp2
+#     Bn   = 0
+#     Bnp1 = 0
+#     Bnp2 = 0
+#     for i in range(p,0,-1):
+#         Bnp2 = Bnp1
+#         Bnp1 = Bn
+#         if i == p:
+#             Bn = np.ones(np.shape(x)) + (x-maxpoly_rec_a[i])*Bnp1 - maxpoly_rec_b[i+1]*Bnp2
+#         else:
+#             Bn = (x-maxpoly_rec_a[i])*Bnp1 - maxpoly_rec_b[i+1]*Bnp2
 
-    return (Bn*(x-2./np.sqrt(np.pi)) - maxpoly_rec_b[1]*Bnp1)/np.sqrt(maxpoly_rec_n[p])
+#     return (Bn*(x-2./np.sqrt(np.pi)) - maxpoly_rec_b[1]*Bnp1)/np.sqrt(maxpoly_rec_n[p])
 
-maxpolyweight = lambda x: 4/np.sqrt(np.pi)*x**2*np.exp(-x**2)
+maxpolyweight = lambda x: x**2*np.exp(-x**2)
+
+# def diff_matrix(G, n):
+
+#     idx = int(G/2)
+
+#     D = np.zeros([n+1, n+1])
+
+#     for j in range(n+1):
+
+#         if j > 0:
+#             D[j-1,j] = j/np.sqrt(maxpoly_beta[idx,j])
+
+#         if j > 1:
+#             D[j-2,j] = (sum(maxpoly_alpha[idx,0:j]) - j*maxpoly_alpha[idx,j-1])/np.sqrt(maxpoly_beta[idx,j]*maxpoly_beta[idx,j-1])
+
+#         if j > 2:
+#             D[j-3,j] = (2.*np.sqrt(maxpoly_beta[idx,j]*maxpoly_beta[idx,j-1]) - np.sqrt(maxpoly_beta[idx,j-1])*D[j-1,j] - maxpoly_alpha[idx,j-2]*D[j-2,j])/np.sqrt(maxpoly_beta[idx,j-2])
+
+#         if j > 3:
+#             for i in range(4,j+1):
+#                 D[j-i,j] = - (np.sqrt(maxpoly_beta[idx,j+2-i])*D[j+2-i,j] + maxpoly_alpha[idx,j+1-i]*D[j+1-i,j])/np.sqrt(maxpoly_beta[idx,j+1-i])
+
+
+#     return D
+
 
 def diff_matrix(G, n):
 
-    idx = int(G/2)
+    idx = G
 
     D = np.zeros([n+1, n+1])
 
     for j in range(n+1):
-
-        if j > 0:
-            D[j-1,j] = j/np.sqrt(maxpoly_beta[idx,j])
-
-        if j > 1:
-            D[j-2,j] = (sum(maxpoly_alpha[idx,0:j]) - j*maxpoly_alpha[idx,j-1])/np.sqrt(maxpoly_beta[idx,j]*maxpoly_beta[idx,j-1])
-
-        if j > 2:
-            D[j-3,j] = (2.*np.sqrt(maxpoly_beta[idx,j]*maxpoly_beta[idx,j-1]) - np.sqrt(maxpoly_beta[idx,j-1])*D[j-1,j] - maxpoly_alpha[idx,j-2]*D[j-2,j])/np.sqrt(maxpoly_beta[idx,j-2])
-
-        if j > 3:
-            for i in range(4,j+1):
-                D[j-i,j] = - (np.sqrt(maxpoly_beta[idx,j+2-i])*D[j+2-i,j] + maxpoly_alpha[idx,j+1-i]*D[j+1-i,j])/np.sqrt(maxpoly_beta[idx,j+1-i])
-
+        for i in range(j):
+            D[i,j] = maxpoly_deriv[idx, int((j-1)*j/2) + i]
 
     return D
 
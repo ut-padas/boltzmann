1208150f18d3f45d3c88fb611cf3d9079c48fb8a
milindasf <milinda@cs.utah.edu>
2022-09-19 10:14:19 -0500

    plotting script

--- BESolver/python/p.py
+++ BESolver/python/p.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df     = pd.read_csv (r'pde_vs_bolsig_08_25_2022.csv')
-df_pic = pd.read_csv (r'PIC_runs.csv')
-
-
+#df        = pd.read_csv (r'pde_vs_bolsig_09_14_2022_collop_approx.csv')
+df_pic    = pd.read_csv (r'PIC_runs.csv')
+fout_name = 'PDE_vs_BOLSIG_G0_G2.png'
 
 r_32  = df[df['Nr']==32]
 r_64  = df[df['Nr']==64]
@@ -26,7 +26,7 @@ e_field_pic = np.array(df_pic['E/N(Td)'])
 r=r_128
 fig = plt.figure(figsize=(21, 9), dpi=300)
 plt.subplot(3, 3,  1)
-plt.plot(e_field_pic, np.array(df_pic['PIC G0']), 'g*', label="PIC g0")
+#plt.plot(e_field_pic, np.array(df_pic['PIC G0']), 'g*', label="PIC g0")
 plt.plot(e_field, np.array(r['bolsig_g0']), 'b*', label="bolsig g0")
 plt.plot(e_field, np.array(r['g0']), 'r*', label="PDE g0")
 plt.xlabel("E/N (Td)")
@@ -38,7 +38,7 @@ plt.legend()
 plt.grid()
 
 plt.subplot(3, 3,  2)
-plt.plot(e_field_pic, np.array(df_pic['PIC G2']), 'g*', label="PIC g2")
+#plt.plot(e_field_pic, np.array(df_pic['PIC G2']), 'g*', label="PIC g2")
 plt.plot(e_field, np.array(r['bolsig_g2']), 'b*', label="bolsig g2")
 plt.plot(e_field, np.array(r['g2']), 'r*', label="PDE g2")
 plt.xlabel("E/N (Td)")
@@ -51,7 +51,7 @@ plt.grid()
 
 
 plt.subplot(3, 3,  3)
-plt.plot(e_field_pic, np.array(df_pic['PIC ENERGY']), 'g*', label="PIC")
+#plt.plot(e_field_pic, np.array(df_pic['PIC ENERGY']), 'g*', label="PIC")
 plt.plot(e_field, np.array(r['bolsig_energy']), 'b*', label="bolsig")
 plt.plot(e_field, np.array(r['energy']), 'r*', label="PDE")
 plt.xlabel("E/N (Td)")
@@ -65,7 +65,7 @@ plt.grid()
 
 plt.subplot(3, 3,  4)
 plt.plot(e_field, np.abs(np.array(r['g0'])/np.array(r['bolsig_g0']) -1), 'r--', label="bolsig vs. PDE (g0)")
-plt.plot(e_field_pic, np.abs(np.array(df_pic['PIC G0'])/np.array(df_pic['BOLSIG G0']) -1), 'b--', label="bolsig vs. PIC(g0)")
+#plt.plot(e_field_pic, np.abs(np.array(df_pic['PIC G0'])/np.array(df_pic['BOLSIG G0']) -1), 'b--', label="bolsig vs. PIC(g0)")
 plt.xlabel("E/N (Td)")
 plt.ylabel("relative error")
 plt.xscale('log')
@@ -77,7 +77,7 @@ plt.grid()
 
 plt.subplot(3, 3,  5)
 plt.plot(e_field, np.abs(np.array(r['g2'])/np.array(r['bolsig_g2']) -1), 'r--', label="bolsig vs. PDE (g0)")
-plt.plot(e_field_pic, np.abs(np.array(df_pic['PIC G2'])/np.array(df_pic['BOLSIG G2']) -1), 'b--', label="bolsig vs. PIC(g2)")
+#plt.plot(e_field_pic, np.abs(np.array(df_pic['PIC G2'])/np.array(df_pic['BOLSIG G2']) -1), 'b--', label="bolsig vs. PIC(g2)")
 plt.xlabel("E/N (Td)")
 plt.ylabel("relative error")
 plt.xscale('log')
@@ -89,7 +89,7 @@ plt.grid()
 
 plt.subplot(3, 3,  6)
 plt.plot(e_field, np.abs(np.array(r['energy'])/np.array(r['bolsig_energy'])-1), 'k--', label="bolsig vs. PDE (energy)")
-plt.plot(e_field_pic, np.abs(np.array(df_pic['PIC ENERGY'])/np.array(df_pic['BOLSIG ENERGY']) -1), 'b--', label="bolsig vs. PIC(energy)")
+#plt.plot(e_field_pic, np.abs(np.array(df_pic['PIC ENERGY'])/np.array(df_pic['BOLSIG ENERGY']) -1), 'b--', label="bolsig vs. PIC(energy)")
 plt.xlabel("E/N (Td)")
 plt.ylabel("relative error")
 plt.xscale('log')
@@ -110,7 +110,7 @@ plt.title("(g)")
 plt.grid()
 
 plt.tight_layout()
-plt.savefig('PDE_vs_BOLSIG_G0_G2.png')
+plt.savefig(fout_name)
 
     
 

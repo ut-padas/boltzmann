40de90bf6d78e7ce93d280fbf336fd5c3778fd5e
milindasf <milinda@cs.utah.edu>
2022-10-21 08:47:59 -0500

    talk and poster initial commit

--- BESolver/python/p.py
+++ BESolver/python/p.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+#plt.rcParams['text.usetex'] = True
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Helvetica",
+    "lines.linewidth":1.2
+})
 
-df     = pd.read_csv (r'pde_vs_bolsig_08_25_2022.csv')
-#df        = pd.read_csv (r'pde_vs_bolsig_09_14_2022_collop_approx.csv')
-df_pic    = pd.read_csv (r'PIC_runs.csv')
-fout_name = 'PDE_vs_BOLSIG_G0_G2.png'
+# df     = pd.read_csv (r'pde_vs_bolsig_08_25_2022.csv')
+# fout_name = 'pde_vs_bolsig_full_collop.png'
+
+df        = pd.read_csv (r'pde_vs_bolsig_09_14_2022_collop_approx.csv')
+fout_name = 'pde_vs_bolsig_collop_approx_eedf.png'
+#df_pic    = pd.read_csv (r'PIC_runs.csv')
+
 
 r_32  = df[df['Nr']==32]
 r_64  = df[df['Nr']==64]
@@ -20,94 +29,91 @@ nr    = ['32', '64', '128']
 #     r.loc[:,'error_g2']=abs(r['g2']/r['bolsig_g2']-1)
 
 
-
+rows = 2
+cols = 3
 e_field = np.array(r_128['E/N(Td)'])
-e_field_pic = np.array(df_pic['E/N(Td)'])
+#e_field_pic = np.array(df_pic['E/N(Td)'])
 r=r_128
 fig = plt.figure(figsize=(21, 9), dpi=300)
-plt.subplot(3, 3,  1)
+plt.subplot(rows, cols,   1)
 #plt.plot(e_field_pic, np.array(df_pic['PIC G0']), 'g*', label="PIC g0")
-plt.plot(e_field, np.array(r['bolsig_g0']), 'b*', label="bolsig g0")
-plt.plot(e_field, np.array(r['g0']), 'r*', label="PDE g0")
-plt.xlabel("E/N (Td)")
-plt.ylabel("reaction rate")
+plt.plot(e_field, np.array(r['bolsig_g0']), 'b*', label="bolsig")
+plt.plot(e_field, np.array(r['g0']), 'rx', label="PDE")
+plt.xlabel(r"E/N (Td)")
+plt.ylabel(r"reaction rate ($m^3s^{-1}$)")
 plt.xscale('log')
 plt.yscale('log')
-plt.title("(a)")
+plt.title(r"Elastic ($e + Ar \rightarrow e + Ar$)")
 plt.legend()
 plt.grid()
 
-plt.subplot(3, 3,  2)
+plt.subplot(rows, cols,   2)
 #plt.plot(e_field_pic, np.array(df_pic['PIC G2']), 'g*', label="PIC g2")
-plt.plot(e_field, np.array(r['bolsig_g2']), 'b*', label="bolsig g2")
-plt.plot(e_field, np.array(r['g2']), 'r*', label="PDE g2")
-plt.xlabel("E/N (Td)")
-plt.ylabel("reaction rate")
+plt.plot(e_field, np.array(r['bolsig_g2']), 'b*', label="bolsig")
+plt.plot(e_field, np.array(r['g2']), 'rx', label="PDE")
+plt.xlabel(r"E/N (Td)")
+plt.ylabel(r"reaction rate ($m^3s^{-1}$)")
 plt.xscale('log')
 plt.yscale('log')
 plt.legend()
-plt.title("(b)")
+plt.title(r"Ionization ($e + Ar \rightarrow 2e + Ar^{+}$)")
 plt.grid()
 
 
-plt.subplot(3, 3,  3)
+plt.subplot(rows, cols,   3)
 #plt.plot(e_field_pic, np.array(df_pic['PIC ENERGY']), 'g*', label="PIC")
 plt.plot(e_field, np.array(r['bolsig_energy']), 'b*', label="bolsig")
-plt.plot(e_field, np.array(r['energy']), 'r*', label="PDE")
-plt.xlabel("E/N (Td)")
-plt.ylabel("energy (eV)")
+plt.plot(e_field, np.array(r['energy']), 'rx', label="PDE")
+plt.xlabel(r"E/N (Td)")
+plt.ylabel(r"energy (eV)")
 plt.xscale('log')
 plt.yscale('log')
 plt.legend()
-plt.title("(c)")
+plt.title(r"steady-state energy")
 plt.grid()
 
 
-plt.subplot(3, 3,  4)
-plt.plot(e_field, np.abs(np.array(r['g0'])/np.array(r['bolsig_g0']) -1), 'r--', label="bolsig vs. PDE (g0)")
+plt.subplot(rows, cols,   4)
+plt.plot(e_field, np.abs(np.array(r['g0'])/np.array(r['bolsig_g0']) -1), 'rx-')
 #plt.plot(e_field_pic, np.abs(np.array(df_pic['PIC G0'])/np.array(df_pic['BOLSIG G0']) -1), 'b--', label="bolsig vs. PIC(g0)")
-plt.xlabel("E/N (Td)")
-plt.ylabel("relative error")
+plt.xlabel(r"E/N (Td)")
+plt.ylabel(r"relative error")
 plt.xscale('log')
 plt.yscale('log')
-plt.title("Elastic")
-plt.legend()
+plt.title(r"Elastic ($e + Ar \rightarrow e + Ar$)")
 plt.title("(d)")
 plt.grid()
 
-plt.subplot(3, 3,  5)
-plt.plot(e_field, np.abs(np.array(r['g2'])/np.array(r['bolsig_g2']) -1), 'r--', label="bolsig vs. PDE (g0)")
+plt.subplot(rows, cols,   5)
+plt.plot(e_field, np.abs(np.array(r['g2'])/np.array(r['bolsig_g2']) -1), 'rx-')
 #plt.plot(e_field_pic, np.abs(np.array(df_pic['PIC G2'])/np.array(df_pic['BOLSIG G2']) -1), 'b--', label="bolsig vs. PIC(g2)")
 plt.xlabel("E/N (Td)")
 plt.ylabel("relative error")
 plt.xscale('log')
 plt.yscale('log')
-plt.legend()
-plt.title("(e)")
+plt.title(r"Ionization ($e + Ar \rightarrow 2e + Ar^{+}$)")
 plt.grid()
 
 
-plt.subplot(3, 3,  6)
-plt.plot(e_field, np.abs(np.array(r['energy'])/np.array(r['bolsig_energy'])-1), 'k--', label="bolsig vs. PDE (energy)")
+plt.subplot(rows, cols,   6)
+plt.plot(e_field, np.abs(np.array(r['energy'])/np.array(r['bolsig_energy'])-1), 'rx-')
 #plt.plot(e_field_pic, np.abs(np.array(df_pic['PIC ENERGY'])/np.array(df_pic['BOLSIG ENERGY']) -1), 'b--', label="bolsig vs. PIC(energy)")
-plt.xlabel("E/N (Td)")
-plt.ylabel("relative error")
+plt.xlabel(r"E/N (Td)")
+plt.ylabel(r"relative error")
 plt.xscale('log')
 plt.yscale('log')
-plt.legend()
-plt.title("(f)")
+plt.title(r"steady-state energy")
 plt.grid()
 
 
-plt.subplot(3, 3,  7)
-plt.plot(e_field, np.array(r['l2_f0']), 'k--', label="bolsig vs. PDE (EEDF f0)")
-plt.xlabel("E/N (Td)")
-plt.ylabel("relative error (l2)")
-plt.xscale('log')
-plt.yscale('log')
-plt.legend()
-plt.title("(g)")
-plt.grid()
+# plt.subplot(rows, cols,   7)
+# plt.plot(e_field, np.array(r['l2_f0']), 'r-')
+# plt.xlabel(r"E/N (Td)")
+# plt.ylabel(r"relative error (l2)")
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.title(r"electron energy density function (EEDF)")
+# plt.grid()
 
 plt.tight_layout()
 plt.savefig(fout_name)

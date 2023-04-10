major cleanup + rosenbluth potential computation quadrature bug fixed

BESolver/python/bolsig.py
@@ -131,20 +131,20 @@ def run_bolsig(args, run_convergence=False):
 
     for i, cc in enumerate(args.collisions):
         if "g0" in str(cc):
-            prefix_line=g0_str
-
-            ev1=np.logspace(-4,4,500,base=10)
-            tcs = collisions.Collisions.synthetic_tcs(ev1,cc)
+            prefix_line = g0_str
+            g           = collisions.eAr_G0(cross_section=cc)
+            ev1         = np.logspace(np.log10(g._energy[0] + 1e-4), np.log10(g._energy[-1]), 800, base=10)
+            tcs         = g.total_cross_section(ev1)
 
             cs_data=np.concatenate((ev1,tcs),axis=0)
             cs_data=cs_data.reshape((2,-1))
             cs_data=np.transpose(cs_data)
 
         elif "g2" in str(cc):
-            prefix_line=g2_str
-
-            ev1=np.logspace(np.log10(15.76),4,500,base=10)
-            tcs = collisions.Collisions.synthetic_tcs(ev1,cc)
+            prefix_line = g2_str
+            g           = collisions.eAr_G2(cross_section=cc)
+            ev1         = np.logspace(np.log10(g._energy[0] + 1e-4), np.log10(g._energy[-1]), 800, base=10)
+            tcs         = g.total_cross_section(ev1)
 
             cs_data=np.concatenate((ev1,tcs),axis=0)
             cs_data=cs_data.reshape((2,-1))

BESolver/python/collisions.py
@@ -53,9 +53,10 @@ PLASMA_FREQUENCY  = np.sqrt(MAXWELLIAN_N * (scipy.constants.elementary_charge**2
 
 class Collisions(abc.ABC):
 
-    def __init__(self)->None:
-        self._is_scattering_mat_assembled=False
-        self._sc_direction_mat=None
+    def __init__(self , cross_section:str)->None:
+        self._is_scattering_mat_assembled = False
+        self._sc_direction_mat            = None
+        self._analytic_cross_section_type = cross_section
         pass
     
     def load_cross_section(self,fname)->None:
@@ -155,7 +156,8 @@ class Collisions(abc.ABC):
         """
         computes the total cross section based on the experimental data. 
         """
-        return self._total_cs_interp1d(energy)
+        return Collisions.synthetic_tcs(energy,self._analytic_cross_section_type)  
+        #return self._total_cs_interp1d(energy)
 
     @staticmethod
     def diff_cs_to_total_cs_ratio(energy,scattering_angle):
@@ -672,10 +674,9 @@ when computing the scattering velocity
 class eAr_G0_NoEnergyLoss(Collisions):
 
     def __init__(self, cross_section="g0") -> None:
-        super().__init__()
+        super().__init__(cross_section)
         self.load_cross_section("lxcat_data/eAr_Elastic.txt")
         self._type=CollisionType.EAR_G0
-        self._analytic_cross_section_type = cross_section
 
     @staticmethod
     def compute_scattering_velocity(v0, polar_angle, azimuthal_angle):
@@ -723,11 +724,10 @@ e + Ar -> e + Ar
 class eAr_G0(Collisions):
 
     def __init__(self, cross_section="g0") -> None:
-        super().__init__()
+        super().__init__(cross_section)
         self.load_cross_section("lxcat_data/eAr_Elastic.txt")
         self._type=CollisionType.EAR_G0
         self._v_scale=None
-        self._analytic_cross_section_type = cross_section
 
     @staticmethod
     def compute_scattering_velocity(v0, polar_angle, azimuthal_angle):
@@ -779,10 +779,9 @@ e + Ar -> e + Ar^*
 class eAr_G1(Collisions):
     
     def __init__(self, cross_section="g1", threshold=E_AR_EXCITATION_eV) -> None:
-        super().__init__()
+        super().__init__(cross_section)
         self.load_cross_section("lxcat_data/eAr_Excitation.txt")
         self._type=CollisionType.EAR_G1
-        self._analytic_cross_section_type = cross_section
         self._reaction_threshold = threshold
 
     @staticmethod
@@ -838,11 +837,10 @@ e + Ar -> e + Ar^+
 class eAr_G2(Collisions):
     
     def __init__(self, cross_section="g2", threshold=E_AR_IONIZATION_eV) -> None:
-        super().__init__()
+        super().__init__(cross_section)
         self.load_cross_section("lxcat_data/eAr_Ionization.txt")
         self._type=CollisionType.EAR_G2
         self._momentum_setup = False
-        self._analytic_cross_section_type = cross_section
         self._reaction_threshold = threshold
         
     def compute_scattering_velocity(self, v0, polar_angle, azimuthal_angle):

BESolver/python/p1.py
@@ -6,37 +6,33 @@ plt.rcParams.update({
     "text.usetex": True,
     "font.family": "Helvetica",
     "font.size": 16,
-    "lines.linewidth":1.2
+    "lines.linewidth":1.2,
+    "lines.markersize" : 4
 })
 
 
 
 def plot_pde_vs_bolsig():
 
-    df          = pd.read_csv (r'pde_vs_bolsig_ee_collisions_lmax1/pde_vs_bolsig_02_04_2023_coulomb_nr64_lmax1.csv')
-    fout_name   = 'pde_vs_bolsig_ee_collisions_lmax1/pde_vs_bolsig_with_coulomb'
+    # df          = pd.read_csv (r'pde_vs_bolsig_ee_collisions_lmax1/pde_vs_bolsig_02_04_2023_coulomb_nr64_lmax1.csv')
+    # fout_name   = 'pde_vs_bolsig_ee_collisions_lmax1/pde_vs_bolsig_with_coulomb'
 
-    df          = pd.read_csv (r'pde_vs_bolsig_ee_collisions_lmax3/pde_vs_bolsig_02_05_2023_coulomb_nr64_lmax3.csv')
-    fout_name   = 'pde_vs_bolsig_ee_collisions_lmax3/pde_vs_bolsig_with_coulomb'
+    df          = pd.read_csv (r'pde_vs_bolsig_ee_collisions_lmax1_03_31_2023/pde_vs_bolsig_03_31_2023_11:44:46.csv')
+    fout_name   = 'pde_vs_bolsig_ee_collisions_lmax1_03_31_2023/pde_vs_bolsig_with_coulomb_collision'
 
     rows = 2
     cols = 2
 
-    for ion_deg in [1e-1,1e-2,1e-3]:
-        # rr_data_lo = df[df['Nr']==64][df['ion_deg']==ion_deg][df['E(V/m)']<1e4]
-        # rr_data_hi = df[df['Nr']==256][df['ion_deg']==ion_deg][df['E(V/m)']>=1e4]
-
-        # rr_data    = pd.concat([rr_data_lo, rr_data_hi], ignore_index=True)
-        # print(rr_data) 
+    fig        = plt.figure(figsize=(21, 9), dpi=300)
+    color_list = ["red","blue", "orange"]
+    for idx, ion_deg in enumerate([1e-1,1e-2,1e-3]):
         rr_data = df[df['Nr']==64.0][df['ion_deg']==ion_deg]
-        print(rr_data)
         e_field = np.array(rr_data['E/N(Td)'])
-
-        fig = plt.figure(figsize=(21, 9), dpi=300)
+        color   = color_list[idx]
+        
         plt.subplot(rows, cols,   1)
-        #plt.plot(e_field_pic, np.array(df_pic['PIC G0']), 'g*', label="PIC g0")
-        plt.plot(e_field, np.array(rr_data['bolsig_g0']), 'b*', label="Bolsig+")
-        plt.plot(e_field, np.array(rr_data['g0']), 'rx', label="PDE")
+        plt.plot(e_field, np.array(rr_data['bolsig_g0']), '.', color=color, label="bolsig ne/n0=%.1E"%(ion_deg))
+        plt.plot(e_field, np.array(rr_data['g0']), 'x'       , color=color, label="pde ne/n0=%.1E"%(ion_deg))
         plt.xlabel(r"E/N (Td)")
         plt.ylabel(r"reaction rate ($m^3s^{-1}$)")
         plt.xscale('log')
@@ -46,87 +42,130 @@ def plot_pde_vs_bolsig():
         plt.grid()
 
         plt.subplot(rows, cols,   2)
-        #plt.plot(e_field_pic, np.array(df_pic['PIC G2']), 'g*', label="PIC g2")
-        plt.plot(e_field, np.array(rr_data['bolsig_g2']), 'b*', label="Bolsig+")
-        plt.plot(e_field, np.array(rr_data['g2']), 'rx', label="PDE")
+        plt.plot(e_field, np.array(rr_data['bolsig_g2']), '.', color=color, label="bolsig ne/n0=%.1E"%(ion_deg))
+        plt.plot(e_field, np.array(rr_data['g2']), 'x'       , color=color, label="pde ne/n0=%.1E"%(ion_deg))
         plt.xlabel(r"E/N (Td)")
         plt.ylabel(r"reaction rate ($m^3s^{-1}$)")
         plt.xscale('log')
         plt.yscale('log')
-        plt.legend()
         plt.title(r"Ionization ($e + Ar \rightarrow 2e + Ar^{+}$)")
         plt.grid()
 
-
         plt.subplot(rows, cols,   3)
-        #plt.plot(e_field_pic, np.array(df_pic['PIC ENERGY']), 'g*', label="PIC")
-        plt.plot(e_field, np.array(rr_data['bolsig_energy']), 'b*', label="Bolsig+")
-        plt.plot(e_field, np.array(rr_data['energy']), 'rx', label="PDE")
+        plt.plot(e_field, np.array(rr_data['bolsig_energy']), '.', color=color, label="bolsig ne/n0=%.1E"%(ion_deg))
+        plt.plot(e_field, np.array(rr_data['energy']), 'x'       , color=color, label="pde ne/n0=%.1E"%(ion_deg))
         plt.xlabel(r"E/N (Td)")
         plt.ylabel(r"energy (eV)")
         plt.xscale('log')
         plt.yscale('log')
-        plt.legend()
         plt.title(r"steady-state energy")
         plt.grid()
 
         plt.subplot(rows, cols,   4)
-        #plt.plot(e_field_pic, np.array(df_pic['PIC ENERGY']), 'g*', label="PIC")
-        plt.plot(e_field, np.array(rr_data['bolsig_mobility']), 'b*', label="Bolsig+")
-        plt.plot(e_field, np.array(rr_data['mobility']), 'rx', label="PDE")
+        plt.plot(e_field, np.array(rr_data['bolsig_mobility']), '.', color=color , label="bolsig ne/n0=%.1E"%(ion_deg))
+        plt.plot(e_field, np.array(rr_data['mobility']), 'x'       , color=color , label="pde ne/n0=%.1E"%(ion_deg))
         plt.xlabel(r"E/N (Td)")
         plt.ylabel(r"mobility (N (1/m/V/s))")
         plt.xscale('log')
         plt.yscale('log')
-        plt.legend()
         plt.title(r"mobility")
         plt.grid()
-
-        fig.suptitle("Gas temperature =%.3E K, ion_deg = %.3E with Nr=%d"%(rr_data.iloc[0]['Tg'],rr_data.iloc[0]['ion_deg'], rr_data.iloc[0]['Nr'])) # or plt.suptitle('Main title')
-
-        plt.tight_layout()
-        plt.savefig("%s_ion_deg_%.3E.png"%(fout_name,ion_deg))
-        plt.close()
+        
 
 
 
-        fig = plt.figure(figsize=(10, 9), dpi=300)
-        #plt.plot(e_field_pic, np.array(df_pic['PIC G0']), 'g*', label="PIC g0")
+    fig.suptitle("Gas temperature =%.2EK, with Nr=%d"%(rr_data.iloc[0]['Tg'], rr_data.iloc[0]['Nr'])) # or plt.suptitle('Main title')
+    plt.tight_layout()
+    plt.savefig("%s.svg"%(fout_name))
+    plt.close()
+
+    rows = 3 
+    cols = 2
+    fig = plt.figure(figsize=(21, 15), dpi=300)
+    color_list = ["red","blue", "orange"]
+    for idx, ion_deg in enumerate([1e-1,1e-2,1e-3]):
+        rr_data = df[df['Nr']==64.0][df['ion_deg']==ion_deg]
+        e_field = np.array(rr_data['E/N(Td)'])
+        color   = color_list[idx]
+
+        plt.subplot(rows, cols,   1)
         w0        = np.array(rr_data['bolsig_g0'])
         w1        = np.array(rr_data['g0'])
         rel_error = np.abs(1-w1/w0)
-        plt.plot(e_field, rel_error, 'r*--', label="elastic")
-
-        w0        = np.array(rr_data['bolsig_g2'])
-        w1        = np.array(rr_data['g2'])
-        rel_error = np.abs(1-w1/w0)
-        rel_error[np.where(w0==0)]=0
-
-        plt.plot(e_field, rel_error, 'b*--', label="ionization")
-
-        w0        = np.array(rr_data['bolsig_energy'])
-        w1        = np.array(rr_data['energy'])
-        rel_error = np.abs(1-w1/w0)
-        plt.plot(e_field, rel_error, 'g*--', label="mean energy")
-
-        w0        = np.array(rr_data['bolsig_mobility'])
-        w1        = np.array(rr_data['mobility'])
-        rel_error = np.abs(1-w1/w0)
-        plt.plot(e_field, rel_error, 'y*--', label="mobility")
-
+        plt.plot(e_field, rel_error, 'x--', color=color ,  label="ne/n0=%.1E"%(ion_deg))
         plt.xlabel(r"E/N (Td)")
-        plt.ylabel(r"relative error")
+        plt.ylabel(r"relative error ")
         plt.xscale('log')
         plt.yscale('log')
+        plt.title(r"Elastic ($e + Ar \rightarrow e + Ar$)")
         plt.legend()
         plt.grid()
 
+        plt.subplot(rows, cols,   2)
+        w0        = np.array(rr_data['bolsig_g2'])
+        w1        = np.array(rr_data['g2'])
+        rel_error = np.abs(1-w1/w0)
+        plt.plot(e_field, rel_error, 'x--', color=color, label="ne/n0=%.1E"%(ion_deg))
+        plt.xlabel(r"E/N (Td)")
+        plt.ylabel(r"relative error ")
+        plt.xscale('log')
+        plt.yscale('log')
+        #plt.legend()
+        plt.title(r"Ionization ($e + Ar \rightarrow 2e + Ar^{+}$)")
+        plt.grid()
 
-        #plt.title("Gas temperature =%.3E, Nr=64 E/N(Td) less than 220 otherwise Nr=256 ne/n0 = %.3E "%(rr_data['Tg'][0],rr_data['ion_deg'][0])) # or plt.suptitle('Main title')
-        plt.suptitle("Gas temperature =%.3E K, ion_deg = %.3E with Nr=%d"%(rr_data.iloc[0]['Tg'],rr_data.iloc[0]['ion_deg'], rr_data.iloc[0]['Nr'])) # or plt.suptitle('Main title')
-        plt.tight_layout()
-        plt.savefig("%s_ion_deg_%.3E_error.png"%(fout_name,ion_deg))
-        plt.close()
+        plt.subplot(rows, cols,   3)
+        w0        = np.array(rr_data['bolsig_energy'])
+        w1        = np.array(rr_data['energy'])
+        rel_error = np.abs(1-w1/w0)
+        plt.plot(e_field, rel_error, 'x--', color=color , label="ne/n0=%.1E"%(ion_deg))
+        plt.xlabel(r"E/N (Td)")
+        plt.ylabel(r"relative error ")
+        plt.xscale('log')
+        plt.yscale('log')
+        #plt.legend()
+        plt.title(r"steady-state energy")
+        plt.grid()
+
+        plt.subplot(rows, cols,   4)
+        w0        = np.array(rr_data['bolsig_mobility'])
+        w1        = np.array(rr_data['mobility'])
+        rel_error = np.abs(1-w1/w0)
+        plt.plot(e_field, rel_error, 'x--', color=color , label="ne/n0=%.1E"%(ion_deg))
+        plt.xlabel(r"E/N (Td)")
+        plt.ylabel(r"relative error ")
+        plt.xscale('log')
+        plt.yscale('log')
+        #plt.legend()
+        plt.title(r"mobility")
+        plt.grid()
+
+        plt.subplot(rows, cols,   5)
+        rel_error = np.array(rr_data['l2_f0'])
+        plt.plot(e_field, rel_error, 'x--', color=color , label="ne/n0=%.1E"%(ion_deg))
+        plt.xlabel(r"E/N (Td)")
+        plt.ylabel(r"relative error ")
+        plt.xscale('log')
+        plt.yscale('log')
+        #plt.legend()
+        plt.title(r"f0")
+        plt.grid()
+
+        plt.subplot(rows, cols,   6)
+        rel_error = np.array(rr_data['l2_f1'])
+        plt.plot(e_field, rel_error, 'x--', color=color , label="ne/n0=%.1E"%(ion_deg))
+        plt.xlabel(r"E/N (Td)")
+        plt.ylabel(r"relative error ")
+        plt.xscale('log')
+        plt.yscale('log')
+        #plt.legend()
+        plt.title(r"f1")
+        plt.grid()
+
+    plt.suptitle("Gas temperature =%.2EK with Nr=%d"%(rr_data.iloc[0]['Tg'], rr_data.iloc[0]['Nr'])) # or plt.suptitle('Main title')
+    plt.tight_layout()
+    plt.savefig("%s_error.svg"%(fout_name))
+    plt.close()
 
 def plot_lmax1_vs_lmax3():
     dir_1 = "pde_vs_bolsig_ee_collisions_lmax1"
@@ -195,7 +234,8 @@ def plot_lmax1_vs_lmax3():
             fig.clear()
             plt.close()
             
-plot_lmax1_vs_lmax3()
+#plot_lmax1_vs_lmax3()
+plot_pde_vs_bolsig()
 
 
     

BESolver/python/utils.py
@@ -111,6 +111,11 @@ def mass_op(spec_sp: spec_spherical.SpectralExpansionSpherical, NUM_Q_VR, NUM_Q_
         MP_klm = np.dot(MP_klm,gmw)
         MP_klm = np.transpose(MP_klm)
         MP_klm = MP_klm.reshape(num_p*num_sph_harm)
+
+        for lm_idx, lm in enumerate(sph_harm_lm):
+            if lm[0]>0:
+                MP_klm[lm_idx::num_sph_harm] = 0.0
+
         mm_g = np.append(mm_g, MP_klm)
 
     MP_klm   = mm_g
@@ -262,6 +267,11 @@ def temp_op(spec_sp: spec_spherical.SpectralExpansionSpherical, NUM_Q_VR, NUM_Q_
         MP_klm = np.dot(MP_klm,gmw)
         MP_klm = np.transpose(MP_klm)
         MP_klm = MP_klm.reshape(num_p*num_sph_harm)
+
+        for lm_idx, lm in enumerate(sph_harm_lm):
+            if lm[0]>0:
+                MP_klm[lm_idx::num_sph_harm] = 0.0
+                
         mm_g   = np.append(mm_g, MP_klm)
 
     MP_klm = mm_g
@@ -732,7 +742,7 @@ def compute_radial_components(ev_pts, spec_sp : spec_spherical.SpectralExpansion
 
     return output
 
-def reaction_rates_op(spec_sp : spec_spherical.SpectralExpansionSpherical, g, mw, vth):
+def reaction_rates_op(spec_sp : spec_spherical.SpectralExpansionSpherical, g_list, mw, vth):
     """
     reaction rate op R for collision g
 
@@ -745,36 +755,90 @@ def reaction_rates_op(spec_sp : spec_spherical.SpectralExpansionSpherical, g, mw
     
     num_p        = spec_sp._p +1
     sph_harm_lm  = params.BEVelocitySpace.SPH_HARM_LM 
-    num_sph_harm = len(sph_harm_lm)
+    num_sh = len(sph_harm_lm)
 
-    gmx,gmw      = spec_sp._basis_p.Gauss_Pn(NUM_Q_VR)
+    gmx_a, gmw_a = spec_sp._basis_p.Gauss_Pn(NUM_Q_VR)
     
-    legendre     = basis.Legendre()
-    [glx,glw]    = legendre.Gauss_Pn(NUM_Q_VT)
-    VTheta_q     = np.arccos(glx)
-    VPhi_q       = np.linspace(0,2*np.pi,NUM_Q_VP)
-
-    assert NUM_Q_VP>1
-    sq_fac_v = (2*np.pi/(NUM_Q_VP-1))
-    WVPhi_q  = np.ones(NUM_Q_VP)*sq_fac_v
-
-    #trap. weights
-    WVPhi_q[0]  = 0.5 * WVPhi_q[0]
-    WVPhi_q[-1] = 0.5 * WVPhi_q[-1]
-
-    quad_grid = np.meshgrid(gmx,VTheta_q,VPhi_q,indexing='ij')
-    P_kr = spec_sp.Vq_r(quad_grid[0], 0 , 1.0) 
-    Y_lm = spec_sp.Vq_sph(quad_grid[1],quad_grid[2])
-
     c_gamma  = np.sqrt(2*scipy.constants.e / scipy.constants.m_e)
-    ev_qx    =  (quad_grid[0] * vth / c_gamma)**2
-    cs_total =  g.total_cross_section(ev_qx)
+    cs_total = 0
+    for g in g_list:
+        cs_total += g.total_cross_section((gmx_a * vth / c_gamma)**2)
+
+    rr_op    = np.zeros(num_p)
     
     if spec_sp.get_radial_basis_type() == basis.BasisType.SPLINES:
-        MP_klm = np.array([((quad_grid[0]**3)) * cs_total * P_kr[i] * Y_lm[j] for i in range(num_p) for j in range(num_sph_harm)])
-        MP_klm = np.dot(MP_klm,WVPhi_q)
-        MP_klm = np.dot(MP_klm,glw)
-        MP_klm = np.dot(MP_klm,gmw) * (2 * vth**4 / (c_gamma**3))
-        return MP_klm
+        k_vec      = spec_sp._basis_p._t
+        dg_idx     = spec_sp._basis_p._dg_idx
+        sp_order   = spec_sp._basis_p._sp_order
+
+        for p in range(num_p):
+            qx_idx = np.logical_and(gmx_a >= k_vec[p], gmx_a <= k_vec[p + sp_order + 1])
+            gmx    = gmx_a[qx_idx]
+            gmw    = gmw_a[qx_idx]
+
+            rr_op[p] = (2 * vth**4 / c_gamma**3) * np.dot(gmw, gmx**3 * cs_total[qx_idx] * spec_sp.basis_eval_radial(gmx, p, 0))
+
+        return rr_op
+    else:
+        raise NotImplementedError
+    
+def mobility_op(spec_sp : spec_spherical.SpectralExpansionSpherical, mw, vth):
+    NUM_Q_VR     = params.BEVelocitySpace.NUM_Q_VR
+    NUM_Q_VT     = params.BEVelocitySpace.NUM_Q_VT
+    NUM_Q_VP     = params.BEVelocitySpace.NUM_Q_VP
+    
+    num_p        = spec_sp._p +1
+    sph_harm_lm  = params.BEVelocitySpace.SPH_HARM_LM 
+    num_sh = len(sph_harm_lm)
+
+    gmx_a, gmw_a = spec_sp._basis_p.Gauss_Pn(NUM_Q_VR)
+    
+    c_gamma  = np.sqrt(2*scipy.constants.e / scipy.constants.m_e)
+    rr_op    = np.zeros(num_p)
+    
+    if spec_sp.get_radial_basis_type() == basis.BasisType.SPLINES:
+        k_vec      = spec_sp._basis_p._t
+        dg_idx     = spec_sp._basis_p._dg_idx
+        sp_order   = spec_sp._basis_p._sp_order
+
+        for p in range(num_p):
+            qx_idx   = np.logical_and(gmx_a >= k_vec[p], gmx_a <= k_vec[p + sp_order + 1])
+            gmx      = gmx_a[qx_idx]
+            gmw      = gmw_a[qx_idx]
+            rr_op[p] = (2 * vth**4 / c_gamma**4) * np.dot(gmw, gmx**3 * spec_sp.basis_eval_radial(gmx, p, 0))
+        return rr_op
+    else:
+        raise NotImplementedError
+    
+def diffusion_op(spec_sp : spec_spherical.SpectralExpansionSpherical, g_list, mw, vth):
+
+    c_gamma      = np.sqrt(2*scipy.constants.e / scipy.constants.m_e)
+    NUM_Q_VR     = params.BEVelocitySpace.NUM_Q_VR
+    NUM_Q_VT     = params.BEVelocitySpace.NUM_Q_VT
+    NUM_Q_VP     = params.BEVelocitySpace.NUM_Q_VP
+    
+    num_p        = spec_sp._p +1
+    sph_harm_lm  = params.BEVelocitySpace.SPH_HARM_LM 
+    num_sh       = len(sph_harm_lm)
+
+    gmx_a, gmw_a = spec_sp._basis_p.Gauss_Pn(NUM_Q_VR)
+    total_cs     = 0
+
+    for g in g_list:
+        total_cs += g.total_cross_section((gmx_a * vth / c_gamma)**2)
+    
+    rr_op    = np.zeros(num_p)
+    
+    if spec_sp.get_radial_basis_type() == basis.BasisType.SPLINES:
+        k_vec      = spec_sp._basis_p._t
+        dg_idx     = spec_sp._basis_p._dg_idx
+        sp_order   = spec_sp._basis_p._sp_order
+
+        for p in range(num_p):
+            qx_idx = np.logical_and(gmx_a >= k_vec[p], gmx_a <= k_vec[p + sp_order + 1])
+            gmx    = gmx_a[qx_idx]
+            gmw    = gmw_a[qx_idx]
+            rr_op[p] = (2 * vth**4 / c_gamma**4) * np.dot(gmw, gmx**3 * spec_sp.basis_eval_radial(gmx, p, 0) / total_cs[qx_idx])
+        return rr_op
     else:
         raise NotImplementedError
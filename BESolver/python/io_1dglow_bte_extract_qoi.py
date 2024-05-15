3dc3378b4bc1ed66da9bf1a8c02f0cdbd2a4321d
milindasf <milinda@oden.utexas.edu>
2024-05-15 11:25:47 -0500

    io extract qoi ccgo4

--- BESolver/python/io_1dglow_bte_extract_qoi.py
+++ BESolver/python/io_1dglow_bte_extract_qoi.py
@@ -23,7 +23,7 @@ def load_data_bte(folder, cycles, eedf_idx=None, read_cycle_avg=False):
     bte_op["temp"]  = np.load(folder+"/1d_glow_bte_temp_op.npy")
     bte_op["g0"]    = np.load(folder+"/1d_glow_bte_op_g0.npy")
     
-    if (int)(sys.argv[3])==1:
+    if (int)(sys.argv[5])==1:
         bte_op["g2"]    = np.load(folder+"/1d_glow_bte_op_g2.npy")
     
     bte_op["po2sh"] = np.load(folder+"/1d_glow_bte_po2sh.npy")
@@ -67,8 +67,8 @@ def compute_rate_coefficients(args, bte_op, v, collisions):
 
 col_names={"g0":"elastic", "g2":"ionization"}
 collisons=["g0"]
-idx_range = list(range(0, (int)(sys.argv[2])))
-if (int)(sys.argv[3])==1:
+idx_range = list(range((int)(sys.argv[2]), (int)(sys.argv[3]), (int)(sys.argv[4]) ))
+if (int)(sys.argv[5])==1:
     collisons.append("g2")
 
 d         = load_data_bte(sys.argv[1], idx_range, None, read_cycle_avg=False)
@@ -76,9 +76,65 @@ ki        = compute_rate_coefficients(d[0], d[3], d[2], collisons)
 for g_idx, g in enumerate(collisons):
     np.save("%s/rates_%s.npy"%(sys.argv[1], col_names[g]),ki[g_idx])
     
+    
+ne=d[1][:, :, 0:2][:,:, 0]
+Te=d[1][:, :, 2]
+print(Te)
+ne = ne[-1]
+Te = Te[-1]
+print("ne", np.min(ne[Te<2]), np.max(ne[Te<2]))
+    
 d         = load_data_bte(sys.argv[1], idx_range, None, read_cycle_avg=True)
 ki        = compute_rate_coefficients(d[0], d[3], d[2], collisons)
 for g_idx, g in enumerate(collisons):
     np.save("%s/rates_avg_%s.npy"%(sys.argv[1], col_names[g]),ki[g_idx])
-    
+
+np.save("%s/species_densities_avg.npy"%(sys.argv[1]), d[1][:, :, 0:2])
+np.save("%s/Te_avg.npy"%(sys.argv[1]), d[1][:,:, 2])
+Np = int(d[0]["Np"])
+
+import matplotlib.pyplot as plt
+from plot_scripts import plot_utils
+xp        = -np.cos(np.pi*np.linspace(0,Np-1,Np) / (Np-1))
+ki_g0_avg = np.load("%s/rates_avg_%s.npy"%(sys.argv[1], col_names["g0"]))
+ki_g2_avg = np.load("%s/rates_avg_%s.npy"%(sys.argv[1], col_names["g2"]))
+ne_avg    = np.load("%s/species_densities_avg.npy"%(sys.argv[1]))[:, : , 0]
+ni_avg    = np.load("%s/species_densities_avg.npy"%(sys.argv[1]))[:, : , 1]
+Te_avg    = np.load("%s/Te_avg.npy"%(sys.argv[1]))
+
+plt.figure(figsize=(8, 8), dpi=300)
+plt.subplot(2, 2, 1)
+plt.semilogy(xp, ne_avg[-1] * 8e16, label=r"$n_e$")
+plt.semilogy(xp, ni_avg[-1] * 8e16, label=r"$n_i$")
+plt.xlabel(r"2x/L -1")
+plt.ylabel(r"number density [$m^{-3}$]")
+plt.grid()
+plt.legend()
+
+plt.subplot(2, 2, 2)
+plt.semilogy(xp, ki_g2_avg[-1], label=r"ionization")
+plt.semilogy(xp, ki_g0_avg[-1], label=r"elastic")
+plt.xlabel(r"2x/L -1")
+plt.ylabel(r"reaction rate [$m^3s^{-1}$]")
+plt.grid()
+plt.legend()
+
+plt.subplot(2, 2, 3)
+plt.semilogy(xp, ki_g2_avg[-1] * ne_avg[-1] * 8e16, label=r"ionization")
+plt.semilogy(xp, ki_g0_avg[-1] * ne_avg[-1] * 8e16, label=r"elastic")
+plt.xlabel(r"2x/L -1")
+plt.ylabel(r"production rate ($k_i n_e$) [$s^{-1}$]")
+plt.grid()
+plt.legend()
+
+plt.subplot(2, 2, 4)
+plt.plot(xp, Te_avg[-1])
+plt.xlabel(r"2x/L -1")
+plt.ylabel(r"temperature [eV]")
+plt.grid()
+
+plt.suptitle(r"$V_0$=100V f=13.56MHz, 3 species model with elastic + ionization")
+plt.tight_layout()
+plt.savefig("%s/test.png"%(sys.argv[1]))
+plt.close()
 

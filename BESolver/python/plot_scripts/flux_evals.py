56f2f70f42a18d9f028a843d9ab9140f68373c86
milindasf <milinda@oden.utexas.edu>
2025-01-12 08:44:17 -0600

    flux evals with scaling to match neuz

--- BESolver/python/plot_scripts/flux_evals.py
+++ BESolver/python/plot_scripts/flux_evals.py
@@ -57,11 +57,21 @@ def time_average(qoi, tt):
     tw    = np.ones_like(tt) * dt
     tw[0] = 0.5 * tw[0]; tw[-1] = 0.5 * tw[-1];
     
-    assert (T-np.sum(tw)) < 1e-12
-    
+    assert np.abs(T-np.sum(tw)) < 1e-12
     return np.dot(tw, qoi)
 
-def extract_flux_and_E(folder, xl, xr):
+def trapz_w(qx):
+    # check if tt is uniform
+    nx   = len(qx)
+    dx   = qx[1:]-qx[0:-1]
+    
+    qw   = np.array([(dx[i-1] + dx[i]) * 0.5 for i in range(1, nx-1)])
+    qw   = np.append(np.array([0.5 * dx[0]]), qw)
+    qw   = np.append(qw, np.array([0.5 * dx[-1]]))
+    assert np.abs((qx[-1] - qx[0])-np.sum(qw)) < 1e-12
+    return qw
+
+def extract_flux_and_E(folder, xl, xr, ev_cutoff=80):
     args   = load_run_args(folder)
     num_l  = int(args["l_max"]) + 1
     vtheta = np.linspace(0, np.pi, 64)
@@ -71,6 +81,7 @@ def extract_flux_and_E(folder, xl, xr):
     xx        = np.array(ff["x[-1,1]"][()])
     tt        = np.array(ff["time[T]"][()])
     ne        = np.array(ff["ne[m^-3]"][()])
+    ni        = np.array(ff["ni[m^-3]"][()])
     fl        = np.array(ff["fl[eV^-1.5]"][()])
     Ef        = np.array(ff["E[Vm^-1]"][()])
     evgrid    = np.array(ff["evgrid[eV]"][()])
@@ -80,17 +91,35 @@ def extract_flux_and_E(folder, xl, xr):
     ux        = np.array(ff["ux[ms^{-1}]"][()])
     L         = (2.54e-2 / 2)
     tau       = 1/13.56e6
+    c_gamma       = np.sqrt(2 * (scipy.constants.elementary_charge/ scipy.constants.electron_mass))
+    # from : https://github.com/ut-padas/boltzmann/blob/main/BESolver/python/qoi_process.py - compute_radial_components
+    vth           = (float) (args["Te"])**0.5 * c_gamma
+    mm_fac        = np.sqrt(4 * np.pi) 
 
     neuz      = ne * uz 
     neuz_avg  = time_average(neuz, tt)
     ne_avg    = time_average(ne, tt)
+    ni_avg    = time_average(ni, tt)
+    E_avg     = time_average(Ef, tt)
 
-    idx       = evgrid<80
+    charge_density = ((ni_avg - ne_avg) / ni_avg)
+    sheath_tol     = 1e-3
+    sidx           = np.argmin(np.abs(charge_density -sheath_tol))
+    print("sheath begin for (ne-ni)/ni rtol = %.4E sheath = %.4E, E_avg = %.4E [V/m]"%(sheath_tol, xx[sidx], E_avg[sidx]))
+    
+    idx       = evgrid<ev_cutoff
     evgrid    = evgrid[idx]
     fl        = fl[:, :, :, idx]
     Np        = len(xx)
     deg       = Np-1
     xp        = -np.cos(np.pi*np.linspace(0,deg,Np)/deg)
+    vr        = np.sqrt(evgrid) * c_gamma 
+    gmx       = vr/vth
+    gmw       = trapz_w(gmx)
+    evw       = trapz_w(evgrid)
+    vthetaw   = trapz_w(vtheta)
+    eedf_const= 2 * (vth/c_gamma)**3  / mm_fac  
+    cos_vt    = np.cos(vtheta)
     assert np.linalg.norm(xp-xx)/np.linalg.norm(xx) < 1e-14, "Chebyshev point mismatch found"
     
 
@@ -105,15 +134,14 @@ def extract_flux_and_E(folder, xl, xr):
     print(xl, xx[xlidx], xr, xx[xridx])
 
     dx      = (xx[xridx] - xx[xlidx]) * L
-    #dne     = tau * np.abs(0.5 * dx * (ip_avg[xlidx] + ip_avg[xridx]) - (neuz_avg[xridx] - neuz_avg[xlidx])) / np.max(ne_avg) 
     dne     = tau * np.abs(0.5 * dx * (ip_avg[xlidx] + ip_avg[xridx]) - (neuz_avg[xridx] - neuz_avg[xlidx])) / (0.5 * dx * (ne_avg[xlidx] + ne_avg[xridx]))
-    print("dne : %.4E"%(dne))
+    print("(prod - flux)/ne_cell = %.8E"%(dne))
     
     #print(ne[-1,:] - ne[0,:])
     #plt.semilogy(xx, np.abs(ne[0,:]-ne[-1,:])/np.max(ne[0,:]))
-    plt.semilogy(xx, np.abs(ne[0,:]-ne[-1,:])/np.abs(ne[0,:]))
-    #plt.semilogy(xx, ne[-1,:])
-    plt.show()
+    # plt.semilogy(xx, np.abs(ne[0,:]-ne[-1,:])/np.abs(ne[0,:]))
+    # #plt.semilogy(xx, ne[-1,:])
+    # plt.show()
 
     #xnew   = np.array([xl, xr])
     xnew   = np.array([xx[xlidx], xx[xridx]])
@@ -121,14 +149,10 @@ def extract_flux_and_E(folder, xl, xr):
 
     flx    = np.einsum("il,albc->aibc", P1, fl)
     nex    = np.einsum("il,al->ai"    , P1, ne)
-
-    c_gamma       = np.sqrt(2 * (scipy.constants.elementary_charge/ scipy.constants.electron_mass))
-    # from : https://github.com/ut-padas/boltzmann/blob/main/BESolver/python/qoi_process.py - compute_radial_components
-    vth           = (float) (args["Te"])**0.5 * c_gamma
-    mm_fac        = np.sqrt(4 * np.pi) 
+    neuzx  = np.einsum("il,al->ai"    , P1, neuz)
+    
     #scale        = np.array([np.dot(mm_op / mm_fac, v_lm[idx]) * (2 * (vth/c_gamma)**3) for idx in range(n_pts)])
-    norm_const    = nex * 2 * (vth/c_gamma)**3  / mm_fac  
-    flx           = np.einsum("ik,iklm->iklm", norm_const, flx)
+    flx           = eedf_const * flx
 
     flx_l         = flx[:, 0, :, :]
     flx_r         = flx[:, 1, :, :]
@@ -137,20 +161,38 @@ def extract_flux_and_E(folder, xl, xr):
 
     flx_l         = np.einsum("iam,al->ilm", flx_l, Vsh)
     flx_r         = np.einsum("iam,al->ilm", flx_r, Vsh)
-    vr            = np.sqrt(evgrid) * c_gamma 
-    cos_vt        = np.cos(vtheta)
+
+    mx_l          = 2 * np.pi * np.dot(np.dot(flx_l * gmx**2 , gmw) * np.sin(vtheta), vthetaw) 
+    mx_r          = 2 * np.pi * np.dot(np.dot(flx_r * gmx**2 , gmw) * np.sin(vtheta), vthetaw)
+
+    flx_l         = np.einsum("t,tar->tar",(1 / mx_l), flx_l)
+    flx_r         = np.einsum("t,tar->tar",(1 / mx_r), flx_r)
+    #print(flx_r.shape)
+    
 
     dvt           = 0.5 * (vtheta[1] - vtheta[0])
     dev           = 0.5 * (evgrid[1] - evgrid[0])
     dvr           = 0.5 * (vr[1] - vr[0])
     extent        = [evgrid[0]-dev , evgrid[-1] + dev, vtheta[0]-dvt, vtheta[-1] + dvt]
     #extent        = [vr[0]-dvr , vr[-1] + dvr, vtheta[0]-dvt, vtheta[-1] + dvt]
-    Sv_l          = np.einsum("a,b,tba->tba", vr, cos_vt, flx_l)
-    Sv_r          = np.einsum("a,b,tba->tba", vr, cos_vt, flx_r)
+    Sv_l          = np.einsum("a,b,tba->tba", gmx * vth, cos_vt, flx_l)
+    Sv_r          = np.einsum("a,b,tba->tba", gmx * vth, cos_vt, flx_r)
 
-    # gmx = vr/vth
-    # print("ne =%.4E from trapz    = %.4E"%(ne[0, xlidx], np.trapezoid(mm_fac * (gmx)**2 * flx[0, 0, 0, :], gmx)))
-    # print("ne uz =%.4E from trapz = %.4E"%(ne[0, xlidx] * uz[0, xlidx], vth * np.trapezoid(np.sqrt(4*np.pi/3) * (gmx)**3 * flx[0, 0, 1, :], gmx)))
+
+    a1_l          = 2 * np.pi * np.dot(np.dot(Sv_l * gmx**2 , gmw) * np.sin(vtheta), vthetaw) * nex[:, 0]
+    a1_r          = 2 * np.pi * np.dot(np.dot(Sv_r * gmx**2 , gmw) * np.sin(vtheta), vthetaw) * nex[:, 1]
+
+    a2_l          = neuzx[:, 0]
+    a2_r          = neuzx[:, 1]
+
+    Sv_l          = np.einsum("t,tar->tar", (a2_l/a1_l) * nex[:, 0], Sv_l)
+    Sv_r          = np.einsum("t,tar->tar", (a2_r/a1_r) * nex[:, 1], Sv_r)
+
+    a1_l          = 2 * np.pi * np.dot(np.dot(Sv_l * gmx**2 , gmw) * np.sin(vtheta), vthetaw) 
+    a1_r          = 2 * np.pi * np.dot(np.dot(Sv_r * gmx**2 , gmw) * np.sin(vtheta), vthetaw) 
+
+    # for tidx in range(0, len(tt), 10):
+    #     print("a1_l = %.8E a2_l = %.8E a1_r=%.8E a2_r=%.8E"%(a1_l[tidx], a2_l[tidx], a1_r[tidx], a2_r[tidx]))
 
     plt.figure(figsize=(12, 4), dpi=200)
     plt.subplot(1, 3, 1)
@@ -217,17 +259,35 @@ xridx  = data["xridx"]
 xx     = data["xx"]
 uz     = data["uz"]
 ne     = data["ne"]
+dx     = xx[xridx] - xx[xlidx]
+L      = 2.54e-2 / 2 
+
+vr_w   = trapz_w(vr)
+vt_w   = trapz_w(vtheta)
+
+print("\
+r1      = ne[tidx, xlidx] * uz[tidx, xlidx]\n\
+a1      = 2*np.pi * np.dot(np.dot(Fvt_L[tidx] * vr**2 , vr_w) * np.sin(vtheta), vt_w)\n\
+r2      = ne[tidx, xridx] * uz[tidx, xridx] \n\
+a2      = 2*np.pi * np.dot(np.dot(Fvt_R[tidx] * vr**2 , vr_w) * np.sin(vtheta), vt_w) \n\
+S       = (a2-a1)\n\
+R       = (r2-r1)")
+
 
 for tidx in range(0, len(tt), 10):
     # left boundary
-    r1     = ne[tidx, xlidx] * uz[tidx, xlidx]
-    a1     = 2*np.pi * np.trapezoid(np.trapezoid(np.einsum("ijk,j->ijk", Fvt_L, np.sin(vtheta))[tidx,:, :], vtheta, axis=0) * vr**2 , vr)
+    r1      = ne[tidx, xlidx] * uz[tidx, xlidx]
+    a1      = 2*np.pi * np.dot(np.dot(Fvt_L[tidx] * vr**2 , vr_w) * np.sin(vtheta), vt_w)
     
     # right boundary
-    r2     = ne[tidx, xridx] * uz[tidx, xridx] 
-    a2     = 2*np.pi * np.trapezoid(np.trapezoid(np.einsum("ijk,j->ijk", Fvt_R, np.sin(vtheta))[tidx,:, :], vtheta, axis=0) * vr**2 , vr)
+    r2      = ne[tidx, xridx] * uz[tidx, xridx] 
+    a2      = 2*np.pi * np.dot(np.dot(Fvt_R[tidx] * vr**2 , vr_w) * np.sin(vtheta), vt_w) 
 
-    print("t = %.4E [s] at left ||uzne - int(fvtL)||/||uzne|| = %.8E  and right ||uzne - int(fvtL)||/||uzne|| = %.8E"%(tt[tidx], np.abs(1-a1/r1), np.abs(1-a2/r2)))
+    S       = (a2-a1)
+    R       = (r2-r1)
+
+    print("a1 = %.8E r1 = %.8E a2 = %.8E r2 = %.8E S=%.8E R=%.8E np.abs(1-S/R) = %.8E"%(a1, r1, a2, r2, S, R, np.abs(1-R/S)))
+    #print("t = %.4E [s] at left ||uzne - int(fvtL)||/||uzne|| = %.8E  and right ||uzne - int(fvtL)||/||uzne|| = %.8E net flux relative error=%.8E "%(tt[tidx], np.abs(1-a1/r1), np.abs(1-a2/r2), np.abs(1-(S/R))))
 
     
     
